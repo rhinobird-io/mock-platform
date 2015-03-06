8073c0d44fab814b6c111501cbcb231460a72b45
Wang Ruijian <wang_r@worksap.co.jp>
2015-03-06 14:59:31 +0800

    Add sudo for docker command

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,9 +9,9 @@ export PLATFORM_DIR=${HOME}/docker/platform/repo
 
 echo -e "Starting ${GW_NAME}..."
 
-DEBIAN_IMAGE="debian:wheezy" && docker history $DEBIAN_IMAGE > /dev/null || echo -e "Downloading $DEBIAN_IMAGE" docker pull $DEBIAN_IMAGE
-docker inspect $GW_NAME >/dev/null && docker rm -f $GW_NAME > /dev/null || true
-docker run -d --name $GW_NAME -v "$HOME":/gateway -w /gateway -p 8000:8000 $DEBIAN_IMAGE ./gateway
+DEBIAN_IMAGE="debian:wheezy" && sudo docker history $DEBIAN_IMAGE > /dev/null || sudo docker pull $DEBIAN_IMAGE
+sudo docker inspect $GW_NAME >/dev/null && sudo docker rm -f $GW_NAME > /dev/null || true
+sudo docker run -d --name $GW_NAME -v "$HOME":/gateway -w /gateway -p 8000:8000 $DEBIAN_IMAGE ./gateway
 export GW_ADDR=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${GW_NAME})
 
 echo -e "Starting ${PLAT_NAME}..."
@@ -30,13 +30,12 @@ fi
   bower install
 )
 
-RUBY_IMAGE="ruby:2.1" && docker history $RUBY_IMAGE > /dev/null || echo -e "Downloading $RUBY_IMAGE" docker pull $RUBY_IMAGE
-docker inspect $PLAT_NAME > /dev/null && docker rm -f $PLAT_NAME > /dev/null || true
+RUBY_IMAGE="ruby:2.1" && sudo docker history $RUBY_IMAGE > /dev/null || sudo docker pull $RUBY_IMAGE
+sudo docker inspect $PLAT_NAME > /dev/null && sudo docker rm -f $PLAT_NAME > /dev/null || true
 (
   cd $PLATFORM_DIR/../
-  docker build -t $PLAT_NAME .
+  sudo docker build -t $PLAT_NAME .
 )
-#docker run --name $PLAT_NAME -v "$PLATFORM_DIR":/app -w /app $RUBY_IMAGE bundle install
-docker run -it --rm --name $PLAT_NAME -v "$PLATFORM_DIR":/app -w /app $PLAT_NAME rake db:migrate
-docker run -d --name $PLAT_NAME -v "$PLATFORM_DIR":/app -w /app -p 4567:4567 -e "AUTH_URL=http://${GW_ADDR}:8000/auth" $PLAT_NAME ruby app.rb
+sudo docker run -it --rm --name $PLAT_NAME -v "$PLATFORM_DIR":/app -w /app $PLAT_NAME rake db:migrate
+sudo docker run -d --name $PLAT_NAME -v "$PLATFORM_DIR":/app -w /app -p 4567:4567 -e "AUTH_URL=http://${GW_ADDR}:8000/auth" $PLAT_NAME ruby app.rb
 
